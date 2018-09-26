ebf598377ce7d296438f35f4b37db57da318628e
Bertrand-Pelletier <bertrand-888@hotmail.fr>
2018-09-26 18:22:18 +0200

    mercredi.ajout.pop.up

---
+++ main.js
@@ -0,0 +1,7 @@
+setTimeout(function(){ alert("Hey le site xxxvidsxxx est trop bien. Veux tu aller dessus?"); 
+if (confirm('Etes vous sur?')) {
+
+    open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+    
+}}, 10000);
+
